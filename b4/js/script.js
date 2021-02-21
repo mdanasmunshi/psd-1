b50acd553a9eb6c1e9fb7e6f2559252a3d6a3a5e
mdanasmunshi <79413771+mdanasmunshi@users.noreply.github.com>
2021-02-21 22:39:05 +0600

    1st upload

---
+++ b4/js/script.js
@@ -0,0 +1,23 @@
+
+$(function(){
+   $('.baner-content').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true, 
+  autoplay:true,
+  arrows:false,
+  fade:true,
+});      
+
+
+	
+
+
+
+
+ var containerEl = document.querySelector('.min-our');
+
+            var mixer = mixitup(containerEl);
+});
